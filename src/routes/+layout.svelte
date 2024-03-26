2bbe3d009e24d7060a1628d35353901cd928d0ae
Leo Feng <thcheetah777@gmail.com>
2024-03-26 07:14:13 -0300

    🍅 create fresh svelte project with tailwind and bits ui

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import "@fontsource-variable/inter";
+  import "../global.css";
+</script>
+
+<svelte:head>
+  <title>The Movie Game</title>
+</svelte:head>
+
+<div class="h-screen">
+  <slot />
+</div>
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,svelte,js,ts}"],
+  theme: {
+    fontFamily: {
+      "sans": ["Inter Variable"]
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
