6a81101dabdf69d52aaa7f2ed98fda64d08c3398
Leo Feng <thcheetah777@gmail.com>
2024-04-02 22:43:51 -0300

    📈 increase random page range by five

--- src/routes/api/random-movie/+server.ts
+++ src/routes/api/random-movie/+server.ts
@@ -4,7 +4,7 @@ import { random, type Movie } from "$lib/utils";
 
 export const GET: RequestHandler = async () => {
   // Get 1 random page of popular movies
-  const pages = 25;
+  const pages = 30;
   const random_page = random(1, pages);
   const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${TMDB_API_KEY}&include_adult=false&with_original_language=en&include_video=false&sort_by=popularity.desc&page=${random_page}`);
   const popular_movies = await response.json();
